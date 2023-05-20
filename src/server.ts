4aa73fb1bd1b5c10e067f9a22da061c4490db1a0
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-05-20 10:59:35 -0300

    auth routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 
@@ -8,6 +9,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true, // todas as URLs de front-end poderão acessar nosso back-end
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(authRoutes)
